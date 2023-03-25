229cfcc54301fd1ed4090485b2254a53a47c9de7
Mirek Simek <miroslav.simek@gmail.com>
2023-03-25 20:38:02 +0100

    Committed automatically after model nr-documents has been installed
    omb-00014-after-model-install-nr-documents

---
+++ models/nr-documents/nr_documents/alembic/5c0a17984d71_create_nr_documents_branch_for_nr_.py
@@ -0,0 +1,30 @@
+import sqlalchemy_utils.types
+import sqlalchemy_utils
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Create nr_documents branch for nr_documents."""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c0a17984d71'
+down_revision = None
+branch_labels = ('nr_documents',)
+depends_on = None
+
+
+def upgrade():
+    """Upgrade database."""
+    pass
+
+
+def downgrade():
+    """Downgrade database."""
+    pass
