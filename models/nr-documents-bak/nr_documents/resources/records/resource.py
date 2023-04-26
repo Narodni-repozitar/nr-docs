4dc79f1ac9f4af73264bb133762a769898285dd7
Alzpetka <lili3.pokorna@seznam.cz>
2023-04-26 15:23:01 +0200

    multilingual dumper

--- models/nr-documents-bak/nr_documents/resources/records/resource.py
+++
@@ -1,9 +0,0 @@
-from invenio_drafts_resources.resources import RecordResource
-from invenio_records_resources.resources import RecordResource
-
-
-class NrDocumentsResource(RecordResource):
-    """NrDocumentsRecord resource."""
-
-    # here you can for example redefine
-    # create_url_rules function to add your own rules
