2d3a80708788fe15c968e5998d93325f5ea4ffbd
Ronald Krist <ronald.krist@cesnet.cz>
2023-07-18 15:24:14 +0200

    Committed automatically before model nr-documents has been compiled
    omb-00107-before-model-compile-nr-documents

--- models/nr-documents/nr_documents/services/records/config.py
+++ models/nr-documents/nr_documents/services/records/config.py
@@ -40,6 +40,8 @@ class NrDocumentsServiceConfig(PermissionsPresetsConfigMixin, RecordServiceConfi
         *InvenioRecordServiceConfig.components,
         *PermissionsPresetsConfigMixin.components,
         *InvenioRecordServiceConfig.components,
+        *PermissionsPresetsConfigMixin.components,
+        *InvenioRecordServiceConfig.components,
     ]
 
     model = "nr_documents"
