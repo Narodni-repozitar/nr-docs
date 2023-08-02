bef9c2156012f07948bab039ec990be2ce42f4ae
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-08-02 12:16:59 +0200

    chore(ui): :wastebasket: remove .assets from git

--- .assets/js/oarepo_ui/search/BucketAggregationElement.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { ContribBucketAggregationElement } from "@js/invenio_search_ui/components";
-
-export const BucketAggregationElement = (props) => {
-  return <ContribBucketAggregationElement {...props} />;
-};
--- .assets/js/oarepo_ui/util.js
+++
@@ -1,8 +0,0 @@
-
-export const getInputFromDOM = (elementName) => {
-    const element = document.getElementsByName(elementName);
-    if (element.length > 0 && element[0].hasAttribute("value")) {
-        return JSON.parse(element[0].value);
-    }
-    return null;
-};
