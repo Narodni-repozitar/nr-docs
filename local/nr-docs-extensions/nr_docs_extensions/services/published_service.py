da3f481a2764df72c47559efe9b22b392775fb89
Ronald Krist <ronald.krist@cesnet.cz>
2023-08-30 14:38:40 +0200

    develop fixes

--- local/nr-docs-extensions/nr_docs_extensions/services/published_service.py
+++ local/nr-docs-extensions/nr_docs_extensions/services/published_service.py
@@ -1,3 +1,5 @@
+import copy
+
 from nr_documents.services.records.config import NrDocumentsServiceConfig
 from oarepo_published_service.services import (
     PublishedService,
@@ -7,8 +9,11 @@ from oarepo_requests.components import PublishDraftComponent, OAICreateRequestsC
 from oarepo_requests.components.requests import PublishDraftComponentPrivate
 from functools import partial
 
+
+class NrDocumentsPublishedServiceConfig(NrDocumentsServiceConfig):
+    components = copy.deepcopy(NrDocumentsServiceConfig.components)
 def get_published_service():
-    config = NrDocumentsServiceConfig()
+    config = NrDocumentsPublishedServiceConfig()
     components = config.components
     for idx, component in enumerate(components):
         if isinstance(component, partial) and component.func == PublishDraftComponentPrivate:
