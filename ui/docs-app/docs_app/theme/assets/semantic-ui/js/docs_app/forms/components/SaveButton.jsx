24d286831f5bba60e1d114c26507c7032c67e9d1
Dusanst <stojanovic@cesnet.cz>
2023-09-04 14:58:23 +0200

    save button new principle

--- ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/SaveButton.jsx
+++ ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/SaveButton.jsx
@@ -3,15 +3,13 @@ import { Button } from "semantic-ui-react";
 import { i18next } from "@translations/docs_app/i18next";
 import { useFormikContext } from "formik";
 import { useSubmitConfig, submitContextType } from "@js/oarepo_ui";
-import {useSubmitSupport} from "@js/oarepo_ui"
-
-
-
+import { useSubmitSupport } from "@js/oarepo_ui";
 
 export const SaveButton = ({ ...uiProps }) => {
   // const { handleSubmit, isSubmitting, setValues, values } = useFormikContext();
-  const {submit, isSubmitting, setValues, values} = useSaveSupport(useFormikContext(), submitContextType.save)
-  const { updateConfig } = useSubmitConfig();
+  const { submit, isSubmitting, setValues, values } = useSubmitSupport(
+    submitContextType.save
+  );
 
   // two functions passed to submitSuccess. One function to manage validation errors that come
   // in the response body. Other one is used to update the record in formik's state with drafts PID
@@ -20,34 +18,6 @@ export const SaveButton = ({ ...uiProps }) => {
   // in invenio, record is not used straight from HTML, but rather it is placed in the store and then from the store
   // passed to formik's initial values, and this allows them to use enableReinitialize prop to reset the form with the
   // newly created PID (entire record actually)
-  const draftSaveSubmitConfig = {
-    context: submitContextType.save,
-    onSubmitSuccess: [
-      (result, formik) => {
-        if (!values.id) {
-          window.history.replaceState(
-            undefined,
-            "",
-            new URL(result.links.self_html).pathname
-          );
-          setValues(result);
-        }
-      },
-      (result, formik) => {
-        if (result.errors) {
-          result.errors?.forEach((err) =>
-            formik.setFieldError(err.field, err.messages.join(" "))
-          );
-          formik.setFieldValue("validationErrors", {
-            errors: result.errors,
-            errorMessage: i18next.t(
-              "Form saved with validation errors. Fields listed below that failed validation were not saved to the server"
-            ),
-          });
-        }
-      },
-    ],
-  };
 
   return (
     <Button
@@ -55,11 +25,8 @@ export const SaveButton = ({ ...uiProps }) => {
       disabled={isSubmitting}
       loading={isSubmitting}
       color="grey"
-      onclick={submit({context: submitContextType.save, ...})}
-      onClick={async () => {
-        await updateConfig(draftSaveSubmitConfig);
-        handleSubmit();
-      }}
+      // onclick={submit({context: submitContextType.save, ...})}
+      onClick={() => submit()()}
       icon="save"
       labelPosition="left"
       content={i18next.t("Save")}
