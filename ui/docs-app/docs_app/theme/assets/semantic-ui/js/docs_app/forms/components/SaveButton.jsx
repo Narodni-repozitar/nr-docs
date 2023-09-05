f58d219891711501daacc2ea424e8ad07c2b93b9
Dusanst <stojanovic@cesnet.cz>
2023-09-05 21:34:07 +0200

    minor changes

--- ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/SaveButton.jsx
+++ ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/SaveButton.jsx
@@ -12,14 +12,6 @@ export const SaveButton = ({ ...uiProps }) => {
     setTimeout(handleSubmit, 0);
   };
 
-  // two functions passed to submitSuccess. One function to manage validation errors that come
-  // in the response body. Other one is used to update the record in formik's state with drafts PID
-  // and links that come in response at the first time draft is created. I did not see more reasonable way to do it (how to
-  // have updated record in the app, as we are not redirecting to another page but only changing history)
-  // in invenio, record is not used straight from HTML, but rather it is placed in the store and then from the store
-  // passed to formik's initial values, and this allows them to use enableReinitialize prop to reset the form with the
-  // newly created PID (entire record actually)
-
   return (
     <Button
       name="save"
