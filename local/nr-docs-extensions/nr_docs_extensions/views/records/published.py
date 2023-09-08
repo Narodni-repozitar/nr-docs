fbc4ff97d594835ba24e5c91bf6162990f521f4e
Ronald Krist <ronald.krist@cesnet.cz>
2023-09-08 16:41:04 +0200

    Committed automatically before model nr-documents has been compiled
    omb-00133-before-model-compile-nr-documents

--- local/nr-docs-extensions/nr_docs_extensions/views/records/published.py
+++ local/nr-docs-extensions/nr_docs_extensions/views/records/published.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from nr_docs_extensions.services.published_service import get_published_service
 
-
 def create_blueprint(app):
     blueprint = Blueprint("nr_documents_published", __name__, url_prefix="")
     blueprint.record_once(init_create_blueprint)
@@ -14,4 +13,6 @@ def init_create_blueprint(state):
 
     # register service
     sregistry = app.extensions["invenio-records-resources"].registry
-    sregistry.register(get_published_service(), service_id="nr_documents_published")
+    sregistry.register(
+        get_published_service(), service_id="nr_documents_published"
+    )
