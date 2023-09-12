6be9b5bd878889a01c2b7cfa034ec7bb327d439d
Ronald Krist <ronald.krist@cesnet.cz>
2023-09-12 09:58:09 +0200

    Committed automatically before model nr-documents has been compiled
    omb-00149-before-model-compile-nr-documents

--- models/nr-documents/nr_documents/records/dumper.py
+++
@@ -1,9 +0,0 @@
-from invenio_records.dumpers import SearchDumper
-
-
-class NrDocumentsDumper(SearchDumper):
-    """NrDocumentsRecord opensearch dumper."""
-
-
-class NrDocumentsDraftDumper(SearchDumper):
-    """NrDocumentsDraft opensearch dumper."""
