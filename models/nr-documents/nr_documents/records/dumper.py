247a7801b57339e531f210b44da7684ef342856d
Ronald Krist <ronald.krist@cesnet.cz>
2023-09-12 09:11:23 +0200

    Committed automatically after model nr-documents has been compiled
    omb-00144-after-model-compile-nr-documents

---
+++ models/nr-documents/nr_documents/records/dumper.py
@@ -0,0 +1,9 @@
+from invenio_records.dumpers import SearchDumper
+
+
+class NrDocumentsDumper(SearchDumper):
+    """NrDocumentsRecord opensearch dumper."""
+
+
+class NrDocumentsDraftDumper(SearchDumper):
+    """NrDocumentsDraft opensearch dumper."""
---
+++ models/nr-documents/nr_documents/services/records/service.py
@@ -0,0 +1,5 @@
+from invenio_drafts_resources.services import RecordService as InvenioRecordService
+
+
+class NrDocumentsService(InvenioRecordService):
+    """NrDocumentsRecord service."""
