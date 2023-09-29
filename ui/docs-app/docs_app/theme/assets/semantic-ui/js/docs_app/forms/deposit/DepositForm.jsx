cf94d261426d4f8b09cc1798507420577687069e
Dusanst <stojanovic@cesnet.cz>
2023-09-29 08:56:04 +0200

    changed order of inputs

--- ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/deposit/DepositForm.jsx
+++ ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/deposit/DepositForm.jsx
@@ -28,7 +28,6 @@ import {
   PublishButtonComponent,
   CreatibutorsField,
 } from "../components/";
-import { VocabularySelectField } from "@js/oarepo_vocabularies";
 import Overridable from "react-overridable";
 import { i18next } from "@translations/docs_app/i18next";
 import _has from "lodash/has";
@@ -82,92 +81,22 @@ export const DepositForm = () => {
               <Overridable id="NrDocs.Deposit.AccordionFieldBasicInformation.container">
                 <AccordionField
                   includesPaths={[
-                    "metadata.resourceType",
                     "metadata.title",
+                    "metadata.additionalTitles",
                     "metadata.creators",
                     "metadata.contributors",
-                    "metadata.additionalTitles",
-                    "metadata.dateModified",
-                    "metadata.dateAvailable",
-                    "metadata.publicationDate",
-                    "metadata.abstract",
-                    "metadata.rights",
                     "metadata.languages",
+                    "metadata.resourceType",
+                    "metadata.abstract",
                     "metadata.accessRights",
-                    "metadata.objectIdentifiers",
-                    "metadata.systemIdentifiers",
+                    "metadata.rights",
+                    "metadata.dateAvailable",
+                    "metadata.dateModified",
                     "metadata.publishers",
                   ]}
                   active
                   label={i18next.t("Basic information")}
                 >
-                  <Overridable
-                    id="NrDocs.Deposit.ObjectIdentifiersField.container"
-                    fieldPath="metadata.objectIdentifiers"
-                  >
-                    <IdentifiersField
-                      options={objectIdentifiersSchema}
-                      fieldPath="metadata.objectIdentifiers"
-                      identifierLabel={i18next.t("Object identifier")}
-                      label={i18next.t("Object identifiers")}
-                      helpText={i18next.t(
-                        "Persistent identifier/s of object as ISBN, DOI, etc."
-                      )}
-                    />
-                  </Overridable>
-                  <Overridable
-                    id="NrDocs.Deposit.SystemIdentifiersField.container"
-                    fieldPath="metadata.systemIdentifiers"
-                  >
-                    <IdentifiersField
-                      options={systemIdentifiersSchema}
-                      fieldPath="metadata.systemIdentifiers"
-                      identifierLabel={i18next.t("System identifier")}
-                      label={i18next.t("System identifiers")}
-                    />
-                  </Overridable>
-
-                  <Overridable
-                    id="NrDocs.Deposit.AccessRightsField.container"
-                    fieldPath="metadata.accessRights"
-                  >
-                    <LocalVocabularySelectField
-                      //TODO: shouldn't access rights be required?
-                      fieldPath="metadata.accessRights"
-                      required
-                      clearable
-                      label={
-                        <FieldLabel
-                          htmlFor={"metadata.accessRights"}
-                          icon="tag"
-                          label={i18next.t("Access rights")}
-                        />
-                      }
-                      placeholder={i18next.t("Select access rights")}
-                      optionsListName="access-rights"
-                    />
-                  </Overridable>
-
-                  <Overridable
-                    id="NrDocs.Deposit.ResourceTypeField.container"
-                    fieldPath="metadata.resourceType"
-                  >
-                    <LocalVocabularySelectField
-                      fieldPath="metadata.resourceType"
-                      required
-                      clearable
-                      label={
-                        <FieldLabel
-                          htmlFor={"metadata.resourceType"}
-                          icon="tag"
-                          label={i18next.t("Resource type")}
-                        />
-                      }
-                      placeholder={i18next.t("Select resource type")}
-                      optionsListName="resource-types"
-                    />
-                  </Overridable>
-
                   <Overridable
                     id="NrDocs.Deposit.TitleField.container"
                     fieldPath="metadata.title"
@@ -190,28 +119,6 @@ export const DepositForm = () => {
                     fieldPath="metadata.additionalTitles"
                   >
                     <AdditionalTitlesField fieldPath="metadata.additionalTitles" />
-                  </Overridable>
-
-                  <Overridable
-                    id="NrDocs.Deposit.DateAvailableField.container"
-                    fieldPath="metadata.dateAvailable"
-                  >
-                    <DateField
-                      fieldPath="metadata.dateAvailable"
-                      label={i18next.t("Date available")}
-                    />
-                  </Overridable>
-                  <Overridable
-                    id="NrDocs.Deposit.DateModifiedField.container"
-                    fieldPath="metadata.dateModified"
-                  >
-                    {editMode && (
-                      <DateField
-                        fieldPath="metadata.dateModified"
-                        label={i18next.t("Date modified")}
-                        helpText=""
-                      />
-                    )}
                   </Overridable>
                   <Overridable
                     id="NrDocs.Deposit.CreatorsField.container"
@@ -244,23 +151,6 @@ export const DepositForm = () => {
                     />
                   </Overridable>
                   <Overridable
-                    id="NrDocs.Deposit.AbstractField.container"
-                    fieldPath="metadata.abstract"
-                  >
-                    <MultilingualTextInput
-                      hasHighlighting
-                      labelIcon="pencil"
-                      label={i18next.t("Abstract")}
-                      textFieldLabel={i18next.t("Description")}
-                      fieldPath="metadata.abstract"
-                      rich={true}
-                      required
-                      helpText={i18next.t(
-                        "Detailed description of the methodology and technical information should be specified in the 'Resource Description' section"
-                      )}
-                    />
-                  </Overridable>
-                  <Overridable
                     id="NrDocs.Deposit.LanguagesField.container"
                     fieldPath="metadata.languages"
                   >
@@ -278,6 +168,62 @@ export const DepositForm = () => {
                       placeholder={i18next.t("Choose languages")}
                       clearable
                       optionsListName="languages"
+                    />
+                  </Overridable>
+                  <Overridable
+                    id="NrDocs.Deposit.ResourceTypeField.container"
+                    fieldPath="metadata.resourceType"
+                  >
+                    <LocalVocabularySelectField
+                      fieldPath="metadata.resourceType"
+                      required
+                      clearable
+                      label={
+                        <FieldLabel
+                          htmlFor={"metadata.resourceType"}
+                          icon="tag"
+                          label={i18next.t("Resource type")}
+                        />
+                      }
+                      placeholder={i18next.t("Select resource type")}
+                      optionsListName="resource-types"
+                    />
+                  </Overridable>
+                  <Overridable
+                    id="NrDocs.Deposit.AbstractField.container"
+                    fieldPath="metadata.abstract"
+                  >
+                    <MultilingualTextInput
+                      hasHighlighting
+                      labelIcon="pencil"
+                      label={i18next.t("Abstract")}
+                      textFieldLabel={i18next.t("Description")}
+                      fieldPath="metadata.abstract"
+                      rich={true}
+                      required
+                      helpText={i18next.t(
+                        "Detailed description of the methodology and technical information should be specified in the 'Resource Description' section"
+                      )}
+                    />
+                  </Overridable>
+                  <Overridable
+                    id="NrDocs.Deposit.AccessRightsField.container"
+                    fieldPath="metadata.accessRights"
+                  >
+                    <LocalVocabularySelectField
+                      //TODO: shouldn't access rights be required?
+                      fieldPath="metadata.accessRights"
+                      required
+                      clearable
+                      label={
+                        <FieldLabel
+                          htmlFor={"metadata.accessRights"}
+                          icon="tag"
+                          label={i18next.t("Access rights")}
+                        />
+                      }
+                      placeholder={i18next.t("Select access rights")}
+                      optionsListName="access-rights"
                     />
                   </Overridable>
                   <Overridable
@@ -301,6 +247,27 @@ export const DepositForm = () => {
                         "If a Creative Commons license is associated with the resource, select the appropriate license option from the menu. We recommend choosing the latest versions, namely 3.0 Czech and 4.0 International."
                       )}
                     />
+                  </Overridable>
+                  <Overridable
+                    id="NrDocs.Deposit.DateAvailableField.container"
+                    fieldPath="metadata.dateAvailable"
+                  >
+                    <DateField
+                      fieldPath="metadata.dateAvailable"
+                      label={i18next.t("Date available")}
+                    />
+                  </Overridable>
+                  <Overridable
+                    id="NrDocs.Deposit.DateModifiedField.container"
+                    fieldPath="metadata.dateModified"
+                  >
+                    {editMode && (
+                      <DateField
+                        fieldPath="metadata.dateModified"
+                        label={i18next.t("Date modified")}
+                        helpText=""
+                      />
+                    )}
                   </Overridable>
                   <Overridable
                     id="NrDocs.Deposit.PublishersField.container"
@@ -468,6 +435,43 @@ export const DepositForm = () => {
                   </Overridable>
                 </AccordionField>
               </Overridable>
+              <Overridable id="NrDocs.Deposit.AccordionFieldIdentifiersInformation.container">
+                <AccordionField
+                  includesPaths={[
+                    "metadata.objectIdentifiers",
+                    "metadata.systemIdentifiers",
+                  ]}
+                  active={false}
+                  label={i18next.t("Identifiers information")}
+                >
+                  <Overridable
+                    id="NrDocs.Deposit.ObjectIdentifiersField.container"
+                    fieldPath="metadata.objectIdentifiers"
+                  >
+                    <IdentifiersField
+                      options={objectIdentifiersSchema}
+                      fieldPath="metadata.objectIdentifiers"
+                      identifierLabel={i18next.t("Object identifier")}
+                      label={i18next.t("Object identifiers")}
+                      helpText={i18next.t(
+                        "Persistent identifier/s of object as ISBN, DOI, etc."
+                      )}
+                    />
+                  </Overridable>
+                  <Overridable
+                    id="NrDocs.Deposit.SystemIdentifiersField.container"
+                    fieldPath="metadata.systemIdentifiers"
+                  >
+                    <IdentifiersField
+                      options={systemIdentifiersSchema}
+                      fieldPath="metadata.systemIdentifiers"
+                      identifierLabel={i18next.t("System identifier")}
+                      label={i18next.t("System identifiers")}
+                    />
+                  </Overridable>
+                </AccordionField>
+              </Overridable>
+
               <FormikStateLogger />
             </Grid.Column>
           </Ref>
