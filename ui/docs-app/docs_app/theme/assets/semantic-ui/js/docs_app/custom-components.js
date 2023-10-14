21c216884f0f8cea4f293673797e146c1ee770dc
ducica <dusanstojanovic256@gmail.com>
2023-10-14 21:45:48 +0200

    added javascript html

--- ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/custom-components.js
+++ ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/custom-components.js
@@ -18,16 +18,14 @@ import { i18next } from "@translations/invenio_app_rdm/i18next";
 import ReactDOM from "react-dom";
 import React from "react";
 
-import "../../less/docs_app/custom-components.less"
-
-
+import "../../less/docs_app/custom-components.less";
 
 /* Expand and collapse navbar  */
 const toggleIcon = $("#invenio-burger-menu-icon");
 const menu = $("#invenio-nav");
 
 toggleIcon.on("click", function () {
-    menu.toggleClass("active");
+  menu.toggleClass("active");
 });
 
 /* Burger menu */
@@ -35,17 +33,17 @@ const $burgerIcon = $("#invenio-burger-menu-icon");
 const $closeBurgerIcon = $("#invenio-close-burger-menu-icon");
 
 const handleBurgerClick = () => {
-    $burgerIcon.attr("aria-expanded", true);
-    $("#invenio-nav").addClass("active");
-    $closeBurgerIcon.trigger("focus");
-    $burgerIcon.css("display", "none");
+  $burgerIcon.attr("aria-expanded", true);
+  $("#invenio-nav").addClass("active");
+  $closeBurgerIcon.trigger("focus");
+  $burgerIcon.css("display", "none");
 };
 
 const handleBurgerCloseClick = () => {
-    $burgerIcon.css("display", "block");
-    $burgerIcon.attr("aria-expanded", false);
-    $("#invenio-nav").removeClass("active");
-    $burgerIcon.trigger("focus");
+  $burgerIcon.css("display", "block");
+  $burgerIcon.attr("aria-expanded", false);
+  $("#invenio-nav").removeClass("active");
+  $burgerIcon.trigger("focus");
 };
 
 $burgerIcon.on({ click: handleBurgerClick });
@@ -54,7 +52,21 @@ $closeBurgerIcon.on({ click: handleBurgerCloseClick });
 const $invenioMenu = $("#invenio-menu");
 
 $invenioMenu.on("keydown", (event) => {
-    if (event.key === "Escape") {
-        handleBurgerCloseClick();
-    }
+  if (event.key === "Escape") {
+    handleBurgerCloseClick();
+  }
 });
+
+const headerSearchbar = document.getElementById("header-search-bar");
+
+if (headerSearchbar) {
+  const searchBarOptions = JSON.parse(headerSearchbar.dataset.options);
+
+  ReactDOM.render(
+    <MultipleOptionsSearchBar
+      options={searchBarOptions}
+      placeholder={i18next.t("Search records...")}
+    />,
+    headerSearchbar
+  );
+}
