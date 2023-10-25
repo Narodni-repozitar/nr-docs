cae641ca8ce517d7b96585c9284b6eda9940a9f6
ducica <dusanstojanovic256@gmail.com>
2023-10-25 22:23:46 +0200

    activ dnd

--- ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/CreatibutorsField/CreatibutorsFieldItem.jsx
+++ ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/CreatibutorsField/CreatibutorsFieldItem.jsx
@@ -64,7 +64,7 @@ export const CreatibutorsFieldItem = ({
     identifiersError.find((elem) => ![undefined, null].includes(elem));
 
   // Initialize the ref explicitely
-  // drop(dropRef);
+  drop(dropRef);
   return (
     <Ref innerRef={dropRef} key={compKey}>
       <List.Item
