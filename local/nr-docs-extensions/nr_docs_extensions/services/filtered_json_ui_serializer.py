49be2585cd1e3e049cf1330dca933068120dbbcd
Mirek Simek <miroslav.simek@gmail.com>
2023-11-01 15:19:32 +0100

    requirements, localized ui serializer

--- local/nr-docs-extensions/nr_docs_extensions/services/filtered_json_ui_serializer.py
+++ local/nr-docs-extensions/nr_docs_extensions/services/filtered_json_ui_serializer.py
@@ -1,8 +1,9 @@
 from flask_resources import MarshmallowSerializer
 from nr_docs_extensions.services.filtered_ui_schema import FilteredUIListSchema
+from oarepo_runtime.resources import LocalizedUIJSONSerializer
 
 
-class FilteredUIJSONSerializer(MarshmallowSerializer):
+class FilteredUIJSONSerializer(LocalizedUIJSONSerializer):
     """UI JSON serializer."""
 
     def __init__(self, **kwargs):
