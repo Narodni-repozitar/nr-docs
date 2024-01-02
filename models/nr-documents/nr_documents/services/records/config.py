6a94e73e8898ddabf257704cf7d834e7a7e41e9a
Mirek Simek <miroslav.simek@gmail.com>
2024-01-02 08:09:08 +0100

    Committed automatically after model nr-documents has been compiled
    omb-00227-after-model-compile-nr-documents

--- models/nr-documents/nr_documents/services/records/config.py
+++ models/nr-documents/nr_documents/services/records/config.py
@@ -3,7 +3,7 @@ from invenio_drafts_resources.services.records.config import is_record
 from invenio_records_resources.services import ConditionalLink, RecordLink
 from invenio_records_resources.services.records.components import DataComponent
 from nr_docs_extensions.services.config import FilteredResultServiceConfig
-from oarepo_requests.components.requests import PublishDraftComponent
+from oarepo_requests.services.components import PublishDraftComponent
 from oarepo_runtime.config.service import PermissionsPresetsConfigMixin
 from oarepo_runtime.services.files import FilesComponent
 from oarepo_runtime.services.results import RecordList
