6ec5377194ebad5c41fb5144cfe567b05659cf18
ducica <dusanstojanovic256@gmail.com>
2024-01-10 09:03:35 +0100

    added version field to publish modal

--- ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/PublishButton/PublishButton.jsx
+++ ui/docs-app/docs_app/theme/assets/semantic-ui/js/docs_app/forms/components/PublishButton/PublishButton.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button, Modal, Message, Icon } from "semantic-ui-react";
+import { Button, Modal, Message, Icon, Form } from "semantic-ui-react";
 import { i18next } from "@translations/docs_app/i18next";
 import { useConfirmationModal, useDepositApiClient } from "@js/oarepo_ui";
+import { TextField, FieldLabel } from "react-invenio-forms";
 import PropTypes from "prop-types";
 
 export const PublishButtonComponent = ({ modalMessage, modalHeader }) => {
@@ -24,6 +25,7 @@ export const PublishButtonComponent = ({ modalMessage, modalHeader }) => {
         fluid
       />
       <Modal
+        className="form-modal"
         open={isModalOpen}
         onClose={handleCloseModal}
         size="small"
@@ -31,15 +33,41 @@ export const PublishButtonComponent = ({ modalMessage, modalHeader }) => {
         closeOnDimmerClick={false}
       >
         <Modal.Header>{modalHeader}</Modal.Header>
-        {modalMessage && (
-          <Modal.Content>
-            <Message visible warning>
-              <p>
-                <Icon name="warning sign" /> {modalMessage}
-              </p>
-            </Message>
-          </Modal.Content>
-        )}
+        <Modal.Content>
+          <Form>
+            <TextField
+              fieldPath="metadata.version"
+              placeholder={i18next.t("Zapište verzi (první, druhá…).")}
+              label={
+                <FieldLabel
+                  htmlFor={"metadata.version"}
+                  icon="pencil"
+                  label={
+                    // <span
+                    //   style={{
+                    //     fontSize: "1.25rem",
+                    //     display: "inline-block",
+                    //     marginBottom: "0.75rem",
+                    //     fontWeight: "bold",
+                    //   }}
+                    // >
+                    i18next.t("Version")
+                    // </span>
+                  }
+                />
+              }
+            />
+          </Form>
+
+          <Message icon size="small">
+            <Icon
+              name="warning sign"
+              size="mini"
+              style={{ fontSize: "1rem" }}
+            />
+            <Message.Content>{modalMessage}</Message.Content>
+          </Message>
+        </Modal.Content>
         <Modal.Actions>
           <Button onClick={handleCloseModal} floated="left">
             {i18next.t("Cancel")}
@@ -71,4 +99,7 @@ PublishButtonComponent.propTypes = {
 
 PublishButtonComponent.defaultProps = {
   modalHeader: i18next.t("Are you sure you wish to publish this draft?"),
+  modalMessage: i18next.t(
+    "Once the record is published you will no longer be able to change the files in the upload! However, you will still be able to update the record's metadata later."
+  ),
 };
