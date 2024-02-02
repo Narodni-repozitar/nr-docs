479ea84220dd9257ccf734a0c819685cc0989121
Mirek Simek <miroslav.simek@gmail.com>
2024-02-02 16:19:15 +0100

    readme

--- scripts/import_data.sh
+++ scripts/import_data.sh
@@ -6,6 +6,14 @@
 
 set -e
 
+# PLEASE SET THE FOLLOWING VARIABLES IN YOUR .envrc.local
+#
+# export NR_DOCS_DUMP_S3_ACCESS_KEY=...
+# export NR_DOCS_DUMP_S3_SECRET_KEY=...
+# export NR_DOCS_DUMP_S3_ENDPOINT_URL=https://s3.cl4.du.cesnet.cz
+# export NR_DOCS_DUMP_S3_HARVEST_NAME=nusl-harvest-02
+# export NR_DOCS_DUMP_S3_BUCKET=nr-repo-docs-harvest
+
 
 # shellcheck disable=SC1090
 source ~/.envrc.local
