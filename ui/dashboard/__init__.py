6f4862676466fd64641a38d29b322cc5ac4b7ebc
ducica <dusanstojanovic256@gmail.com>
2024-02-06 12:17:04 +0100

    added entry points

--- ui/dashboard/__init__.py
+++ ui/dashboard/__init__.py
@@ -1,5 +1,7 @@
 from oarepo_ui.resources.config import TemplatePageUIResourceConfig
 from oarepo_ui.resources.resource import TemplatePageUIResource
+from invenio_search_ui.searchconfig import FacetsConfig, SearchAppConfig, SortConfig
+
 
 
 class DashboardPageResourceConfig(TemplatePageUIResourceConfig):
@@ -13,11 +15,44 @@ class DashboardPageResourceConfig(TemplatePageUIResourceConfig):
         # add a new page here. The key is the URL path, the value is the name of the template
         # then put <name>.jinja into the templates folder
     }
+    
+    
+    def records_search_app_config(self, overrides={}, **kwargs):
+        opts = dict(
+            endpoint="/api/documents",
+            headers={"Accept": "application/vnd.inveniordm.v1+json"},
+            grid_view=False,
+            sort=SortConfig(
+            {
+        "title": dict(
+            title=("By Title"),
+            fields=["metadata.title"],  # ES defaults to desc on `_score` field
+        ),
+        "bestmatch": dict(
+            title=("Best match"),
+            fields=["_score"],  # ES defaults to desc on `_score` field
+        ),
+        "newest": dict(
+            title=("Newest"),
+            fields=["-created"],
+        ),
+        "oldest": dict(
+            title=("Oldest"),
+            fields=["created"],
+        ),
+    }, {}, "oldest", "oldest"
+        ),
+            facets={},
+            
+        )
+        opts.update(kwargs)
+        return SearchAppConfig.generate(opts, **overrides)
 
 
 class DashboardPageResource(TemplatePageUIResource):
     def render_DashboardRecordsPage(self, **kwargs):
-        return self.render("DashboardRecordsPage", **kwargs)
+        search_app_config=self.config.records_search_app_config()
+        return self.render("DashboardRecordsPage", search_app_config=search_app_config , **kwargs)
     
     def render_DashboardCommunitiesPage(self, **kwargs):
         return self.render("DashboardCommunitiesPage", **kwargs)
