fcece3b07306cb6bca78dabcdbd578d642610a17
ducica <dusanstojanovic256@gmail.com>
2024-02-20 11:37:04 +0100

    fixed broken dependencies and styles

--- common/services/published_service.py
+++ common/services/published_service.py
@@ -1,7 +1,5 @@
-from oarepo_published_service.services import (
-    PublishedServiceConfig
-)
-from oarepo_requests.services.components import PublishDraftComponentPrivate
+from oarepo_published_service.services import PublishedServiceConfig
+# from oarepo_requests.services.components import PublishDraftComponentPrivate
 
 from functools import partial
 
@@ -9,10 +7,16 @@ from oarepo_runtime.services.config.service import PermissionsPresetsConfigMixin
 
 
 # todo: do this better, edit this on the lvel of published service config
-class NrDocumentsPublishedServiceConfig(PublishedServiceConfig, PermissionsPresetsConfigMixin):
+class NrDocumentsPublishedServiceConfig(
+    PublishedServiceConfig, PermissionsPresetsConfigMixin
+):
     @property
     def components(self):
         return [
-            component for component in super().components
-            if not (isinstance(component, partial) and component.func == PublishDraftComponentPrivate)
+            component
+            for component in super().components
+            # if not (
+            #     isinstance(component, partial)
+            #     and component.func == PublishDraftComponentPrivate
+            # )
         ]
