3d3d0e46050db8a26f25ecd5f13d738c84849a82
Juraj Trappl <juraj.trappl@proton.me>
2024-03-28 20:38:04 +0100

    move debug repo code outside of common

--- debug_repository.py
+++ debug_repository.py
@@ -1,4 +1,19 @@
 from invenio_app.cli import cli
 
 if __name__ == "__main__":
-    cli.main()
+    cli.main(
+        # args=[
+        #     "run",
+        #     "--cert",
+        #     "./docker/development.crt",
+        #     "--key",
+        #     "./docker/development.key",
+        # ]
+        args=[
+            "oarepo",
+            "oai",
+            "harvester",
+            "run",
+            "nusl_s3_composite",
+        ]
+    )
