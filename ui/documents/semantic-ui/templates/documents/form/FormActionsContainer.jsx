2769b7d31cd5edb60505e99c232edf217ea8dabd
Ducica <34883098+Ducica@users.noreply.github.com>
2024-04-26 12:31:44 +0200

    Preview page (#102)
    * importing from @nr/search and @nr/forms
    
    * publishbtn from oarepo + translations
    
    * added translations
    
    * added is_preview prop to detail
    
    * fixed spacer on smaller screens
    
    * integrated files cmp to detail page
    
    * added tree element to theme.config
    
    * nrp upgrade

--- ui/documents/semantic-ui/templates/documents/form/FormActionsContainer.jsx
+++ ui/documents/semantic-ui/templates/documents/form/FormActionsContainer.jsx
@@ -8,6 +8,8 @@ import {
   useFormConfig,
   PublishButton,
 } from "@js/oarepo_ui";
+import { TextField, FieldLabel } from "react-invenio-forms";
+import { i18next } from "@translations/i18next";
 
 const FormActionsContainer = () => {
   const { links } = useFormConfig();
@@ -27,7 +29,23 @@ const FormActionsContainer = () => {
           </Grid.Column>
 
           <Grid.Column width={16} className="pt-10">
-            <PublishButton />
+            <PublishButton
+              additionalInputs={
+                <TextField
+                  fieldPath="metadata.version"
+                  placeholder={i18next.t(
+                    "Write the version (first, second ...)."
+                  )}
+                  label={
+                    <FieldLabel
+                      htmlFor={"metadata.version"}
+                      icon="pencil"
+                      label={i18next.t("Version")}
+                    />
+                  }
+                />
+              }
+            />
           </Grid.Column>
           <Grid.Column width={16} className="pt-10">
             <ValidateButton />
