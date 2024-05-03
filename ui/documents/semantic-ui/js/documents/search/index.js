ff8f785ce509243b924dee1891667ae2f6843532
ducica <dusanstojanovic256@gmail.com>
2024-05-03 13:01:19 +0200

    added histogram to searchapp cmps

--- ui/documents/semantic-ui/js/documents/search/index.js
+++ ui/documents/semantic-ui/js/documents/search/index.js
@@ -2,12 +2,15 @@ import {
   createSearchAppsInit,
   parseSearchAppConfigs,
   SearchappSearchbarElement,
+  HistogramWSlider,
 } from "@js/oarepo_ui";
 
 const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
 export const componentOverrides = {
   [`${overridableIdPrefix}.SearchBar.element`]: SearchappSearchbarElement,
+  [`${overridableIdPrefix}.BucketAggregation.element.date_issued_histogram`]:
+    HistogramWSlider,
 };
 
 createSearchAppsInit({ componentOverrides });
