3853af4fc423f8a1a44b33a57f5487db2fcf9d5b
AlbinaMakisheva <allielane24@gmail.com>
2024-06-03 21:00:09 +0200

    fixed rand

--- tests/ui/util.js
+++ tests/ui/util.js
@@ -1,7 +1,13 @@
 const crypto = require('crypto');
 
-export const getRandomInt = async (max) => {
+export const getRandomInt =  (max) => {
   const randomBytes = crypto.randomBytes(4);
-  const randomValue = randomBytes.readUInt32BE(0);
-  return randomValue % max;
+  const randomNumber = randomBytes.readUInt32BE(0);
+
+  const unbiasedMax = Math.floor((2 ** 32) / max) * max;
+
+  if (randomNumber < unbiasedMax) {
+    return randomNumber % max;
+  } 
 };
+
