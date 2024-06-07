a70b85211d907f6190175c9bd4fc630288d55fe2
ducica <dusanstojanovic256@gmail.com>
2024-06-07 13:39:08 +0200

    foldable facets

--- ui/documents/semantic-ui/js/documents/search/index.js
+++ ui/documents/semantic-ui/js/documents/search/index.js
@@ -2,12 +2,15 @@ import {
   createSearchAppsInit,
   parseSearchAppConfigs,
   SearchappSearchbarElement,
+  FoldableBucketAggregationElement,
 } from "@js/oarepo_ui";
 
 const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
 export const componentOverrides = {
   [`${overridableIdPrefix}.SearchBar.element`]: SearchappSearchbarElement,
+  [`${overridableIdPrefix}.BucketAggregation.element`]:
+    FoldableBucketAggregationElement,
 };
 
 createSearchAppsInit({ componentOverrides });
