129878d2d2c337fba773bf7e9c1983f1a0a47b78
ducica <dusanstojanovic256@gmail.com>
2024-06-11 09:55:47 +0200

    removed unused import

--- ui/documents/semantic-ui/js/documents/search/index.js
+++ ui/documents/semantic-ui/js/documents/search/index.js
@@ -2,15 +2,12 @@ import {
   createSearchAppsInit,
   parseSearchAppConfigs,
   SearchappSearchbarElement,
-  FoldableBucketAggregationElement,
 } from "@js/oarepo_ui";
 
 const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
 export const componentOverrides = {
   [`${overridableIdPrefix}.SearchBar.element`]: SearchappSearchbarElement,
-  [`${overridableIdPrefix}.BucketAggregation.element`]:
-    FoldableBucketAggregationElement,
 };
 
 createSearchAppsInit({ componentOverrides });
