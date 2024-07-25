dc660322dfdacd43f5596c9e23f5a51b510efac4
Juraj Trappl <juraj.trappl@proton.me>
2024-07-25 14:24:12 +0200

    register the new writer

--- common/config.py
+++ common/config.py
@@ -1,5 +1,6 @@
 from common.oai.readers.s3 import S3Reader
 from common.oai.writers.composite import CompositeWriter
+from common.oai.writers.timestamp_update import TimestampUpdateWriter
 
 DATASTREAMS_READERS = { "s3": S3Reader }
-DATASTREAMS_WRITERS = { "composite": CompositeWriter }
+DATASTREAMS_WRITERS = { "composite": CompositeWriter, "timestamp_update": TimestampUpdateWriter }
