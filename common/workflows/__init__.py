930f0f83c3b147561f9b9a76b87264ebf128dcea
Mirek Simek <miroslav.simek@gmail.com>
2024-08-16 15:30:47 +0200

    Replaced workflow permission policy with community workflow permission policy

--- common/workflows/__init__.py
+++ common/workflows/__init__.py
@@ -1,9 +1,5 @@
-from invenio_records_permissions.generators import AnyUser, AuthenticatedUser
+from oarepo_communities.services.permissions.policy import CommunityWorkflowPermissionPolicy
 
-from oarepo_workflows.services.permissions.policy import WorkflowPermissionPolicy
 
-class DocsCommunitiesPermissionPreset(WorkflowPermissionPolicy):
-    can_view_deposit_page = [
-        # TODO: should be removed after this permission is implemented directly on oarepo-communities
-        AuthenticatedUser(),
-    ]
+class DocsCommunitiesPermissionPreset(CommunityWorkflowPermissionPolicy):
+    pass
