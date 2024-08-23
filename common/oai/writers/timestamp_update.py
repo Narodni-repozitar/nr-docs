b27cb052ab5871947b5e1116bb319d4f203b0375
Juraj Trappl <juraj.trappl@proton.me>
2024-08-23 15:27:18 +0200

    correct timestamp update writer

--- common/oai/writers/timestamp_update.py
+++ common/oai/writers/timestamp_update.py
@@ -1,9 +1,11 @@
 from invenio_access.permissions import system_identity
 from invenio_db import db
-from invenio_pidstore.errors import PIDDoesNotExistError
 from invenio_records_resources.proxies import current_service_registry
-from sqlalchemy import Table, update
+from invenio_records_resources.services.uow import UnitOfWork
+from invenio_search.engine import dsl
+from sqlalchemy import Table, update, select
 
+from invenio_records_resources.services.uow import RecordIndexOp
 from oarepo_runtime.datastreams.types import StreamBatch
 from oarepo_runtime.datastreams.writers import BaseWriter
 
@@ -17,27 +19,31 @@ class TimestampUpdateWriter(BaseWriter):
         self._identity = identity or system_identity
 
     def write(self, batch: StreamBatch) -> StreamBatch:
-        for entry in batch.ok_entries:
-            if entry.deleted:
-                continue
+        with UnitOfWork() as uow:
+            for entry in batch.ok_entries:
+                if entry.deleted:
+                    continue
 
-            read_entry = self._service.read(system_identity, entry.id)
+                record = self._service.read(system_identity, entry.id)
 
-            model = read_entry._record.model
-            table = Table(model.__tablename__, model.metadata, autoload_with=db.engine)
-            stmt = (
-                update(table)
-                .where(table.c.id == model.id)
-                .values(
-                    updated=entry.context["oai"]["datestamp"],
-                    created=entry.context["oai"]["datestamp"],
+                model = record._record.model
+                table = Table(model.__tablename__, model.metadata)
+                stmt = (
+                    update(table)
+                    .where(table.c.id == model.id)
+                    .values(
+                        updated=record.data["oai"]["harvest"]["datestamp"],
+                        created=record.data["oai"]["harvest"]["datestamp"],
+                    )
                 )
-            )
-            db.session.execute(stmt)
-            db.session.commit()
+                db.session.execute(stmt)
 
+                record1 = self._service.read(system_identity, entry.id)
+
+                uow.register(RecordIndexOp(record1._record, indexer=self._service.indexer, index_refresh=True))
+
+            uow.commit()
+        
+        db.session.commit()
         db.session.expunge_all()
-        self._service.reindex(
-            system_identity,
-            {"query": {"ids": {"values": [entry.id for entry in batch.ok_entries]}}},
-        )
+
