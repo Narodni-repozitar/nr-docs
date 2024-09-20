87a4d6c90e65a4a9e46e999a4521f07a383cd668
Juraj Trappl <juraj.trappl@proton.me>
2024-09-20 16:50:52 +0200

    add new publish writer

--- common/config.py
+++ common/config.py
@@ -1,7 +1,14 @@
-from oarepo_runtime.datastreams.writers.attachments_service import AttachmentsServiceWriter
+from oarepo_runtime.datastreams.writers.attachments_service import (
+    AttachmentsServiceWriter,
+)
 
 from common.oai.readers.s3 import S3Reader
 from common.oai.writers.timestamp_update import TimestampUpdateWriter
+from common.oai.writers.publish_writer import PublishWriter
 
-DATASTREAMS_READERS = { "s3": S3Reader }
-DATASTREAMS_WRITERS = { "attachment": AttachmentsServiceWriter, "timestamp_update": TimestampUpdateWriter }
+DATASTREAMS_READERS = {"s3": S3Reader}
+DATASTREAMS_WRITERS = {
+    "attachment": AttachmentsServiceWriter,
+    "timestamp_update": TimestampUpdateWriter,
+    "publish": PublishWriter,
+}
