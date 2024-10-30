9fbfa60398eff969769aff95d783a4d93cc3ea03
Alzbeta Pokorna <alzbetapokorna@eduroam-tlh85.cesnet.cz>
2024-10-30 15:14:01 +0100

    rdm service config

--- common/services/config.py
+++ common/services/config.py
@@ -1,9 +1,7 @@
-from invenio_drafts_resources.services import (
-    RecordServiceConfig as InvenioRecordDraftsServiceConfig,
-)
 
 from common.services.filtered_record_list import FilteredRecordList
+from invenio_rdm_records.services.config import RDMRecordServiceConfig
 
 
-class FilteredResultServiceConfig(InvenioRecordDraftsServiceConfig):
+class FilteredResultServiceConfig(RDMRecordServiceConfig):
     result_list_cls = FilteredRecordList
