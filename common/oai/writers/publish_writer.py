bd957844aa7afeed9ead9942290bad851bb4cd4d
Juraj Trappl <juraj.trappl@proton.me>
2024-11-10 00:44:17 +0100

    add formatting of the error

--- common/oai/writers/publish_writer.py
+++ common/oai/writers/publish_writer.py
@@ -3,7 +3,7 @@ from invenio_records_resources.proxies import current_service_registry
 
 from oarepo_runtime.datastreams.types import StreamBatch
 from oarepo_runtime.datastreams.writers import BaseWriter
-
+from oarepo_runtime.datastreams.writers.utils import record_invenio_exceptions 
 
 class PublishWriter(BaseWriter):
     def __init__(self, *, service, identity=None):
@@ -23,19 +23,20 @@ class PublishWriter(BaseWriter):
             if entry.deleted:
                 continue
 
-            draft = self._service.read_draft(system_identity, entry.id)
-            request = current_oarepo_requests_service.create(
-                identity=system_identity,
-                data=None,
-                request_type="publish_draft",
-                topic=draft._record,
-            )
+            with record_invenio_exceptions(entry):
+                draft = self._service.read_draft(system_identity, entry.id)
+                request = current_oarepo_requests_service.create(
+                    identity=system_identity,
+                    data=None,
+                    request_type="publish_draft",
+                    topic=draft._record,
+                )
 
-            submit_result = current_invenio_requests_service.execute_action(
-                system_identity, request.id, "submit"
-            )
-            accept_result = current_invenio_requests_service.execute_action(
-                system_identity, request.id, "accept"
-            )
+                submit_result = current_invenio_requests_service.execute_action(
+                    system_identity, request.id, "submit"
+                )
+                accept_result = current_invenio_requests_service.execute_action(
+                    system_identity, request.id, "accept"
+                )
 
-            self._service.read(system_identity, draft["id"])
+                self._service.read(system_identity, draft["id"])
