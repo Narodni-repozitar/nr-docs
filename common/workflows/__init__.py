a78db7d7f4572b9d0b4b913b414878c136d25311
ducica <dusanstojanovic256@gmail.com>
2025-02-14 14:59:07 +0100

    removed permission to managerecordaccess

--- common/workflows/__init__.py
+++ common/workflows/__init__.py
@@ -1,9 +1,7 @@
 from oarepo_communities.services.permissions.policy import (
     CommunityWorkflowPermissionPolicy,
 )
-from invenio_records_permissions.generators import AnyUser, SystemProcess
 
 
 class DocsCommunitiesPermissionPreset(CommunityWorkflowPermissionPolicy):
-    can_manage_record_access = [AnyUser()]
-    can_lift_embargo = [AnyUser(), SystemProcess()]
+    pass
