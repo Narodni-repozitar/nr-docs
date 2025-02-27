13874de15d7478f8370a1f63438f48a0b40566f3
ducica <dusanstojanovic256@gmail.com>
2025-02-27 10:23:23 +0100

    fixed result list item on /docs

--- ui/documents/semantic-ui/js/documents/search/index.js
+++ ui/documents/semantic-ui/js/documents/search/index.js
@@ -1,5 +1,13 @@
 import {
+  parseSearchAppConfigs,
   createSearchAppsInit,
 } from "@js/oarepo_ui/search";
+import ResultsListItem from "./ResultsListItem";
 
-createSearchAppsInit();
+const [{ overridableIdPrefix }] = parseSearchAppConfigs();
+
+export const componentOverrides = {
+  [`${overridableIdPrefix}.ResultsList.item`]: ResultsListItem,
+};
+
+createSearchAppsInit({ componentOverrides });
