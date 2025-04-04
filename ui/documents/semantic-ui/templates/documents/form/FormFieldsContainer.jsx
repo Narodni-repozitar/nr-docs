8c23843fb152e221aa5a453600bf41d4ce87b8fa
Ducica <34883098+Ducica@users.noreply.github.com>
2025-04-04 14:17:00 +0200

    minimizing props to components (#280)
    * minimizing props to components
    
    * tree select, related items and licenses
    
    * identifiers field from oarepo ui

--- ui/documents/semantic-ui/templates/documents/form/FormFieldsContainer.jsx
+++ ui/documents/semantic-ui/templates/documents/form/FormFieldsContainer.jsx
@@ -4,10 +4,10 @@ import {
   MultilingualTextInput,
   FormikStateLogger,
   EDTFSingleDatePicker,
-  useFieldData,
-  useSanitizeInput,
   FilesField,
   StringArrayField,
+  TextField,
+  IdentifiersField,
   FundingField,
   CreatibutorsField,
 } from "@js/oarepo_ui/forms";
@@ -16,14 +16,13 @@ import {
   LocalVocabularySelectField,
   VocabularyTreeSelectField,
 } from "@js/oarepo_vocabularies";
-import { AccordionField, TextField } from "react-invenio-forms";
+import { AccordionField } from "react-invenio-forms";
 import {
   AdditionalTitlesField,
   ExternalLocationField,
   SubjectsField,
   SeriesField,
   EventsField,
-  IdentifiersField,
   RelatedItemsField,
   objectIdentifiersSchema,
   LicenseField,
@@ -31,7 +30,6 @@ import {
 import Overridable from "react-overridable";
 import { i18next } from "@translations/i18next";
 import _has from "lodash/has";
-import { useFormikContext, getIn } from "formik";
 
 const FormFieldsContainer = () => {
   const { formConfig, files: recordFiles } = useFormConfig();
@@ -44,10 +42,6 @@ const FormFieldsContainer = () => {
       ),
     []
   );
-  const { getFieldData } = useFieldData();
-
-  const { values, setFieldValue, setFieldTouched } = useFormikContext();
-  const { sanitizeInput } = useSanitizeInput();
 
   return (
     <React.Fragment>
@@ -64,7 +58,6 @@ const FormFieldsContainer = () => {
             "metadata.languages",
             "metadata.dateIssued",
             "metadata.publishers",
-            "metadata.accessRights",
             "metadata.rights",
             "metadata.dateModified",
           ]}
@@ -75,18 +68,7 @@ const FormFieldsContainer = () => {
             id="NrDocs.Deposit.TitleField.container"
             fieldPath="metadata.title"
           >
-            <TextField
-              optimized
-              fieldPath="metadata.title"
-              {...getFieldData({ fieldPath: "metadata.title" })}
-              onBlur={() => {
-                const cleanedContent = sanitizeInput(
-                  getIn(values, "metadata.title")
-                );
-                setFieldValue("metadata.title", cleanedContent);
-                setFieldTouched("metadata.title", true);
-              }}
-            />
+            <TextField fieldPath="metadata.title" />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.AdditionalTitlesField.container"
@@ -99,16 +81,9 @@ const FormFieldsContainer = () => {
             fieldPath="metadata.resourceType"
           >
             <LocalVocabularySelectField
-              optimized
               fieldPath="metadata.resourceType"
-              multiple={false}
               filterFunction={submissibleResourceTypes}
-              clearable
               optionsListName="resource-types"
-              {...getFieldData({
-                fieldPath: "metadata.resourceType",
-                icon: "tag",
-              })}
             />
           </Overridable>
           <Overridable
@@ -125,28 +100,15 @@ const FormFieldsContainer = () => {
             fieldPath="metadata.languages"
           >
             <LocalVocabularySelectField
-              optimized
               fieldPath="metadata.languages"
-              multiple={true}
-              clearable
               optionsListName="languages"
-              {...getFieldData({
-                fieldPath: "metadata.languages",
-                icon: "language",
-              })}
             />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.DateIssuedField.container"
             fieldPath="metadata.dateIssued"
           >
-            <EDTFSingleDatePicker
-              fieldPath="metadata.dateIssued"
-              {...getFieldData({
-                fieldPath: "metadata.dateIssued",
-                icon: "calendar",
-              })}
-            />
+            <EDTFSingleDatePicker fieldPath="metadata.dateIssued" />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.PublishersField.container"
@@ -155,49 +117,20 @@ const FormFieldsContainer = () => {
             <StringArrayField
               fieldPath="metadata.publishers"
               addButtonLabel={i18next.t("Add publisher")}
-              {...getFieldData({ fieldPath: "metadata.publishers" })}
             />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.LicenseField.container"
             fieldPath="metadata.rights"
           >
-            <LicenseField
-              searchConfig={{
-                searchApi: {
-                  axios: {
-                    headers: {
-                      Accept: "application/vnd.inveniordm.v1+json",
-                    },
-                    url: "/api/vocabularies/rights",
-                  },
-                },
-                initialQueryState: {
-                  size: 25,
-                  page: 1,
-                  sortBy: "bestmatch",
-                  filters: [["tags", ""]],
-                },
-              }}
-              fieldPath="metadata.rights"
-              {...getFieldData({
-                fieldPath: "metadata.rights",
-                icon: "drivers license",
-              })}
-            />
+            <LicenseField fieldPath="metadata.rights" />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.DateModifiedField.container"
             fieldPath="metadata.dateModified"
           >
             {editMode && (
-              <EDTFSingleDatePicker
-                fieldPath="metadata.dateModified"
-                {...getFieldData({
-                  fieldPath: "metadata.dateModified",
-                  icon: "calendar",
-                })}
-              />
+              <EDTFSingleDatePicker fieldPath="metadata.dateModified" />
             )}
           </Overridable>
         </AccordionField>
@@ -256,25 +189,14 @@ const FormFieldsContainer = () => {
           >
             <VocabularyTreeSelectField
               fieldPath="metadata.subjectCategories"
-              multiple={true}
               vocabulary="subject-categories"
-              {...getFieldData({
-                fieldPath: "metadata.subjectCategories",
-                icon: "tag",
-              })}
             />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.AbstractField.container"
             fieldPath="metadata.abstract"
           >
-            <MultilingualTextInput
-              textFieldLabel={i18next.t("Description")}
-              fieldPath="metadata.abstract"
-              rich={true}
-              lngFieldWidth={4}
-              {...getFieldData({ fieldPath: "metadata.abstract" })}
-            />
+            <MultilingualTextInput fieldPath="metadata.abstract" rich={true} />
           </Overridable>
           <Overridable
             id="NrDocs.Deposit.SeriesField.container"
@@ -295,7 +217,6 @@ const FormFieldsContainer = () => {
             <StringArrayField
               addButtonLabel={i18next.t("Add note")}
               fieldPath="metadata.notes"
-              {...getFieldData({ fieldPath: "metadata.notes" })}
             />
           </Overridable>
         </AccordionField>
@@ -323,10 +244,7 @@ const FormFieldsContainer = () => {
             id="NrDocs.Deposit.RelatedItemsField.container"
             fieldPath="metadata.relatedItems"
           >
-            <RelatedItemsField
-              fieldPath="metadata.relatedItems"
-              {...getFieldData({ fieldPath: "metadata.relatedItems" })}
-            />
+            <RelatedItemsField fieldPath="metadata.relatedItems" />
           </Overridable>
         </AccordionField>
       </Overridable>
