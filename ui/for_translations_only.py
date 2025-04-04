56d2eaae57e73764c517d933001e27ab3a96d4fa
ducica <dusanstojanovic256@gmail.com>
2025-04-04 10:05:13 +0200

    fixed and translated role names

---
+++ ui/for_translations_only.py
@@ -0,0 +1,11 @@
+from oarepo_runtime.i18n import lazy_gettext as _
+
+
+_("Supercurator")
+_("Can manage community.")
+_("Curator")
+_("Can curate records.")
+_("Submitter")
+_("Can submit records.")
+_("Member")
+_("Community member with read permissions.")
