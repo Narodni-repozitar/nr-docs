8886628116093c3a717458aaa540dd704833534f
Mirek Simek <miroslav.simek@gmail.com>
2025-04-15 20:57:53 +0200

    Fixed ids in awards & names, removed publish writer as it already is in oarepo-runtime

--- common/config.py
+++ common/config.py
@@ -1,17 +1,14 @@
-from flask_resources import HTTPJSONException, create_error_handler
 from oarepo_runtime.datastreams.writers.attachments_service import (
     AttachmentsServiceWriter,
 )
 
 from common.oai.readers.s3 import S3Reader
-from common.oai.writers.timestamp_update import TimestampUpdateWriter
-from common.oai.writers.publish_writer import PublishWriter
 from common.oai.writers.ownership_writer import OwnershipWriter
+from common.oai.writers.timestamp_update import TimestampUpdateWriter
 
 DATASTREAMS_READERS = {"s3": S3Reader}
 DATASTREAMS_WRITERS = {
     "attachment": AttachmentsServiceWriter,
     "owner": OwnershipWriter,
-    "publish": PublishWriter,
     "timestamp_update": TimestampUpdateWriter,
 }
