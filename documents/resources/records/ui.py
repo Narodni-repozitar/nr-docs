373f2c843c8dd11b4a053623be98f270bba1457a
ducica <dusanstojanovic256@gmail.com>
2025-06-25 13:33:00 +0200

    fix: added filters_l10n to search results

--- documents/resources/records/ui.py
+++ documents/resources/records/ui.py
@@ -2,18 +2,26 @@ from flask import g
 from flask_resources import BaseListSchema
 from flask_resources.serializers import JSONSerializer
 
-from common.services.filtered_json_ui_serializer import FilteredUIJSONSerializer
+from oarepo_runtime.resources import (
+    FilteredUIJSONSerializer,
+)
 from documents.services.records.ui_schema import DocumentsUISchema
 
 
 class DocumentsUIJSONSerializer(FilteredUIJSONSerializer):
     """UI JSON serializer."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
+        base_schema_context = {"object_key": "ui", "service": "documents"}
+        schema_context = (
+            base_schema_context | kwargs["schema_context"]
+            if "schema_context" in kwargs
+            else base_schema_context
+        )
         """Initialise Serializer."""
         super().__init__(
             format_serializer_cls=JSONSerializer,
             object_schema_cls=DocumentsUISchema,
             list_schema_cls=BaseListSchema,
-            schema_context={"object_key": "ui", "identity": g.identity},
+            schema_context=schema_context,
         )
