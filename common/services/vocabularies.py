b898f08f3952a65fe85bc5a7057c513ebf0c5ee7
Mirek Simek <miroslav.simek@gmail.com>
2025-07-31 12:24:02 +0200

    Replaced QueryStrParam with suggestion-based param

--- common/services/vocabularies.py
+++ common/services/vocabularies.py
@@ -1,5 +1,10 @@
+import logging
+
 from invenio_records_resources.services.records.params.base import ParamInterpreter
+from invenio_records_resources.services.records.params.querystr import QueryStrParam
 from opensearch_dsl.query import Bool, Term
+
+log = logging.getLogger(__name__)
 
 
 class DeprecatedVocabularyParamInterpreter(ParamInterpreter):
@@ -10,37 +15,58 @@ class DeprecatedVocabularyParamInterpreter(ParamInterpreter):
         return search.filter(Bool(must_not=[Term(tags="deprecated")]))
 
 
-def remove_deprecated_vocabulary_items_from_search(app):
-    """Add deprecated items removal to vocabulary search."""
-    with app.app_context():
-        from invenio_records_resources.proxies import current_service_registry
-        from oarepo_vocabularies.services.search import VocabularySearchOptions
+class SuggestionOnlyQueryStrParam(QueryStrParam):
+    """Evaluate the 'q' or 'suggest' parameter."""
 
-        # oarepo extensions might not have been applied at this point,
-        # so we need to patch both the running service and the search options
+    def apply(self, identity, search, params):
+        """Evaluate the query str on the search."""
+
+        if "q" in params and "/" in params["q"] and '"' not in params["q"]:
+            params["suggest"] = params.pop("q")
+        return super().apply(identity, search, params)
+
+
+def update_vocabulary_search_options():
+    """Update search options to include deprecated items removal."""
+
+    def remove_deprecated_from_search(search_options):
         if (
             DeprecatedVocabularyParamInterpreter
-            not in VocabularySearchOptions.params_interpreters_cls
+            not in search_options.params_interpreters_cls
         ):
-            VocabularySearchOptions.params_interpreters_cls.append(
+            search_options.params_interpreters_cls.append(
                 DeprecatedVocabularyParamInterpreter
             )
 
-        def remove_deprecated_from_search(service):
-            search_options = service.config.search
-            if (
-                DeprecatedVocabularyParamInterpreter
-                not in search_options.params_interpreters_cls
-            ):
-                search_options.params_interpreters_cls.append(
-                    DeprecatedVocabularyParamInterpreter
+    def replace_querystr_with_suggestion_only(search_options):
+        for idx, p in enumerate(search_options.params_interpreters_cls):
+            if p is QueryStrParam:
+                search_options.params_interpreters_cls[idx] = (
+                    SuggestionOnlyQueryStrParam
                 )
 
-        try:
-            remove_deprecated_from_search(current_service_registry.get("vocabularies"))
-            remove_deprecated_from_search(current_service_registry.get("affiliations"))
-            remove_deprecated_from_search(current_service_registry.get("awards"))
-            remove_deprecated_from_search(current_service_registry.get("funders"))
-        except KeyError:
-            # If the service is not registered, it means it is not available in this context.
-            pass
+    from invenio_vocabularies.contrib.affiliations.config import (
+        SearchOptions as AffiliationsSearchOptions,
+    )
+
+    remove_deprecated_from_search(AffiliationsSearchOptions)
+    replace_querystr_with_suggestion_only(AffiliationsSearchOptions)
+
+    from invenio_vocabularies.contrib.funders.config import (
+        SearchOptions as FundersSearchOptions,
+    )
+
+    remove_deprecated_from_search(FundersSearchOptions)
+    replace_querystr_with_suggestion_only(FundersSearchOptions)
+
+    from invenio_vocabularies.contrib.awards.config import (
+        SearchOptions as AwardsSearchOptions,
+    )
+
+    remove_deprecated_from_search(AwardsSearchOptions)
+    replace_querystr_with_suggestion_only(AwardsSearchOptions)
+
+    from oarepo_vocabularies.services.search import VocabularySearchOptions
+
+    remove_deprecated_from_search(VocabularySearchOptions)
+    replace_querystr_with_suggestion_only(VocabularySearchOptions)
