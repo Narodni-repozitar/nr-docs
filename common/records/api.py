ae662e6ae59bcb7ccdb95879c9cd5b470d25140f
Juraj Trappl <juraj.trappl@proton.me>
2025-08-13 19:01:34 +0200

    remove new common stuff

--- common/records/api.py
+++
@@ -1,7 +0,0 @@
-from invenio_drafts_resources.records.api import DraftRecordIdProviderV2
-from oarepo_runtime.records.pid_providers import UniversalPIDMixin
-
-from common.records.pid_providers import ReuseNUSLMixin
-
-class DocumentsIdProvider(UniversalPIDMixin, ReuseNUSLMixin, DraftRecordIdProviderV2):
-    pid_type = "dcmnts"
